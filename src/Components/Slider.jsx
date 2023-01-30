32d0f544b9c6efdefcb5e370b0320cbbd6b9ac51
Óscar O <oscarforjuela@hotmail.com>
2023-01-29 23:14:49 -0500

    New Images and Carousel

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -5,29 +5,31 @@ import imagenes from "../Images/imagenes";
 const Slider = () => {
     return (
         <>
-            <Carousel>
-                <Carousel.Item>
-                    <img className="d-block" width={900} src={imagenes.logo} alt="La imagen no se encuentra disponible"/>
-                    <Carousel.Caption>
-                        <h3>Repositorio UNO</h3>
-                        <p>Descripción</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img className="d-block w-80" src={imagenes.logo} alt="La imagen no se encuentra disponible"/>
-                    <Carousel.Caption>
-                        <h3>Repositorio DOS</h3>
-                        <p>Descripción</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img className="d-block w-80" src={imagenes.logo} alt="La imagen no se encuentra disponible"/>
-                    <Carousel.Caption>
-                        <h3>Repositorio TRES</h3>
-                        <p>Descripción</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>            
+            <div className="container">
+                <Carousel>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={imagenes.Repo1} alt="La imagen no se encuentra disponible" />
+                        <Carousel.Caption>
+                            <h3>Mortal Dev</h3>
+                            <p>Juego desarrollado durante el Botcamp <a href="https://mortaldev.netlify.app/">Juégalo aquí</a></p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-80" src={imagenes.logo} alt="La imagen no se encuentra disponible" />
+                        <Carousel.Caption>
+                            <h3>Repositorio DOS</h3>
+                            <p>Descripción</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-80" src={imagenes.logo} alt="La imagen no se encuentra disponible" />
+                        <Carousel.Caption>
+                            <h3>Repositorio TRES</h3>
+                            <p>Descripción</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </>
     );
 };
--- src/Images/imagenes.js
+++ src/Images/imagenes.js
@@ -1,5 +1,7 @@
 import LOGO from './LOGO.png';
+import Repo1 from './RepoMortalDev.png';
 
 export default {
-    "logo": LOGO
+    "logo": LOGO,
+    "Repo1": Repo1
 }
