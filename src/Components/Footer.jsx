b868411d321f4c482a31dc3d01fe44db8083c543
Óscar O <oscarforjuela@hotmail.com>
2023-02-05 16:18:27 -0500

    New Icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () =>{
         <>
         <footer>
             
+            
         </footer>        
         </>
     );
--- src/Images/imagenes.js
+++ src/Images/imagenes.js
@@ -2,10 +2,14 @@ import LOGO from './LOGO.png';
 import Repo1 from './RepoMortalDev.png';
 import Repo2 from './RepoTetris.png';
 import Git from './github.png';
+import Mail from './mail.png';
+import Linkedin from './linkedin.png';
 
 export default {
     "logo": LOGO,
     "Repo1": Repo1,
     "Repo2": Repo2,
-    "Git": Git
+    "Git": Git,
+    "Mail": Mail,
+    "Linkedin": Linkedin
 }
