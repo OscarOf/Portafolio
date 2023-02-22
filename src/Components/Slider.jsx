ff5a73b5d84c9b15d4c63509300d6b62602a0feb
Óscar O <oscarforjuela@hotmail.com>
2023-02-22 10:54:20 -0500

    New images and New Repo

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -11,22 +11,22 @@ const Slider = () => {
                         <img className="d-block w-100" src={imagenes.Repo1} alt="La imagen no se encuentra disponible" />
                         <Carousel.Caption>
                             <h3>Mortal Dev</h3>
-                            <p>Juego desarrollado durante el Botcamp <a href="https://mortaldev.netlify.app/">Juégalo aquí</a></p>
+                            <p>Juego desarrollado durante el Botcamp, tecnologías usadas: Javascript, htmnl y css <a href="https://mortaldev.netlify.app/">Juégalo aquí</a></p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="d-block w-100" src={imagenes.Repo2} alt="La imagen no se encuentra disponible" />
                         <Carousel.Caption>
                             <h3>Tetris JS</h3>
-                            <p>Este proyecto lo realizamos mediante un tutorial de Youtube, es mi primer reto con Javascript aplicando Canva.
+                            <p>Este proyecto lo realizamos mediante un tutorial de Youtube, era nuestro primer reto con Javascript aplicando Canva.
                             <a href="https://tetris-r85x.vercel.app"> Pruébalo</a></p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="d-block w-80" src={imagenes.logo} alt="La imagen no se encuentra disponible" />
                         <Carousel.Caption>
-                            <h3>Repositorio TRES</h3>
-                            <p>Descripción</p>
+                            <h3>MovieCRUD</h3>
+                            <p>Este fue nuestro primer  proyecto de nodeJS, el cual se implementa Express, MongoDB y React</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
--- src/Images/imagenes.js
+++ src/Images/imagenes.js
@@ -1,6 +1,7 @@
 import LOGO from './LOGO.png';
 import Repo1 from './RepoMortalDev.png';
 import Repo2 from './RepoTetris.png';
+import Repo3 from './RepoMovies.png';
 import Git from './github.png';
 import Mail from './mail.png';
 import Linkedin from './linkedin.png';
@@ -9,6 +10,7 @@ export default {
     "logo": LOGO,
     "Repo1": Repo1,
     "Repo2": Repo2,
+    "Repo3": Repo3,
     "Git": Git,
     "Mail": Mail,
     "Linkedin": Linkedin
